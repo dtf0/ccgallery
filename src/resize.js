fad4db885588fc62a55ad8f858d5fb01936b29c3
Jason Baker <jason@onejasonforsale.com>
2026-05-30 22:31:47 -0700

    Refactor imagegallery.js into js classes

--- src/resize.js
+++ src/resize.js
@@ -6,16 +6,16 @@ const sharp = require('sharp');
 // node path reference: https://nodejs.org/api/path.html
 // sharp image library reference: https://sharp.pixelplumbing.com/
 
-async function resizeImage(originalFilePath, thumbnailFilePath) {  
-  let image = sharp(originalFilePath);
-
-  // resize image to auto-width (first 'null' arg) given height of 100
-  await image.resize(null, 100, (err) => {
-    console.log("resize error", err)
-  }); 
-
-  // save image as jpg
-  await image.jpeg({ mozjpeg:true }).toFile(thumbnailFilePath);
+async function resizeImage(originalFilePath, thumbnailFilePath) { 
+  //resize image to auto-width (first 'null' arg) given height of 100 
+  console.log("b:" + originalFilePath)
+  await sharp(originalFilePath)
+    .resize(null, 100, (err) => {
+      console.log("resize error", err)
+    })
+    .jpeg({ mozjpeg:true })
+    .toFile(thumbnailFilePath);
+  console.log("c:" + originalFilePath)
 };
 
 async function getImageDims(file) {
@@ -46,6 +46,28 @@ function isSupportedImageFile(file) {
   return false;
 }
 
+function isDirectory(path) {
+  try {
+    if (fs.existsSync(inputDirectory)) {
+      return fs.statSync(path).isDirectory();
+    }    
+  } catch (error) {
+    console.log("error checking if dir: " + path, error)
+  }
+  return false;
+}
+
+function makeDirectory(path) {
+  try {
+    if (isDirectory(path)) {
+      return;
+    }
+    fs.mkdirSync(path, { "recursive":true });
+  } catch (error) {
+    console.log("error making dir: " + path, error)
+  }
+}
+
 async function getImageMetaData(originalFilePath, thumbnailFilePath) {
   let originalMetaData = await getImageDims(originalFilePath);
   originalMetaData.file = path.parse(originalFilePath).base;
@@ -61,7 +83,9 @@ async function getImageMetaData(originalFilePath, thumbnailFilePath) {
   return imageMetaData;
 }
 
-async function processImages(inputDirectory, outputDirectory) {
+async function processGalleryImages(galleryName, inputDirectory, outputDirectory) {
+  console.log("Processing images for gallery '" + galleryName + "'",  { "inputDirectory":inputDirectory, "outputDirectory":outputDirectory });
+  
   if (!fs.existsSync(inputDirectory)) {
     console.log("Input directory does not exist: " + inputDirectory)
     return;
@@ -71,8 +95,6 @@ async function processImages(inputDirectory, outputDirectory) {
     console.log("Output directory does not exist: " + outputDirectory)
     return;
   }
-
-  console.log("Processing images in '" + inputDirectory + "', thumbnails will be in '" + outputDirectory + "'");
 
   const inputFiles = fs.readdirSync(inputDirectory);
   // console.log("input files", inputFiles);  
@@ -91,14 +113,16 @@ async function processImages(inputDirectory, outputDirectory) {
     let thumbnailFile = originalFilePathInfo.name + "_tn" + originalFilePathInfo.ext;
     let thumbnailFilePath = outputDirectory + "/" + thumbnailFile;
 
-    console.log("Processing '" + originalFilePath + "' -> '" + thumbnailFilePath + "'");
+    console.log("Processing '" + galleryName + "/" + originalFile + "'");
 
     // copy input file to gallery directory
     let copiedFilePath = outputDirectory + "/" + originalFile;
-    await fs.copyFileSync(originalFilePath, copiedFilePath);
+    fs.copyFileSync(originalFilePath, copiedFilePath);
 
     // create resized thumbnail in gallery directory
+    console.log("a:" + copiedFilePath)
     await resizeImage(copiedFilePath, thumbnailFilePath);
+    console.log("d:" + copiedFilePath)
 
     // save image metadata
     let imageMetaData = await getImageMetaData(copiedFilePath, thumbnailFilePath);
@@ -107,9 +131,59 @@ async function processImages(inputDirectory, outputDirectory) {
 
   let jsonFile = outputDirectory + "/" + "galleries.json";
 
-  await writeJSON(jsonFile, imageMetaDatas);
+  let galleryMetaData = { "name":galleryName, "images":imageMetaDatas };
+
+  await writeJSON(jsonFile, galleryMetaData);
 
   console.log("Wrote galleries JSON file:"  + jsonFile);
+}
+
+async function findGalleries(inputDirectory) {
+  let galleries = [];
+  const inputFiles = fs.readdirSync(inputDirectory);
+
+  let rootGalleryExists = false;
+
+  for (file of inputFiles) {
+    let filePath = inputDirectory + "/" + file;
+    if (isSupportedImageFile(filePath)) {
+      rootGalleryExists = true;
+    } else if (isDirectory(filePath)) {
+      galleries.push({ "name":file, "path":filePath })
+    }
+  }
+
+  if (rootGalleryExists) {
+    galleries.push({ "name":".", "path":inputDirectory + "/." })
+  }
+  return galleries;
+}
+
+async function processGalleries(inputDirectory, outputDirectory) {
+  console.log("Processing Galleries", { "inputDirectory":inputDirectory, "outputDirectory":outputDirectory });
+
+  if (!fs.existsSync(inputDirectory)) {
+    console.log("Input directory does not exist: " + inputDirectory)
+    return;
+  }
+
+  let galleryNames = [];
+  let galleries = await findGalleries(inputDirectory);
+  console.log("galleries", galleries);
+  for (gallery of galleries) {
+    let galleryOutputDirectory = outputDirectory + "/" + gallery.name;
+    console.log("Now processing gallery: '" + gallery.name + "', path: " + galleryOutputDirectory);
+    if (!isDirectory(galleryOutputDirectory)) {
+      makeDirectory(galleryOutputDirectory);
+    }
+    await processGalleryImages(gallery.name, gallery.path, galleryOutputDirectory);
+    galleryNames.push(gallery.name);
+  }
+
+  let jsonFile = outputDirectory + "/" + "galleries-index.json";
+  let galleryMetaData = { "galleryNames":galleryNames };
+  await writeJSON(jsonFile, galleryMetaData);
+  console.log("Wrote galleries index JSON file:"  + jsonFile);
 }
 
 if (process.argv.length < 3) {
@@ -120,17 +194,7 @@ if (process.argv.length < 3) {
 var inputDirectory = process.argv[2];
 var outputDirectory = process.argv[3];
 
-if (!fs.existsSync(inputDirectory)) {
-  console.log("Input directory does not exist: " + inputDirectory)
-  return;
-}
-
-if (!fs.existsSync(outputDirectory)) {
-  console.log("Output directory does not exist: " + outputDirectory)
-  return;
-}
-
-processImages(inputDirectory, outputDirectory);
+processGalleries(inputDirectory, outputDirectory);
 
 
 
--- src/web/imagegallery.js
+++ src/web/imagegallery.js
@@ -1,273 +1,363 @@
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-console.log("isMobile: " + isMobile);
-
-var onImageLoadOpacity = isMobile ? 1.0 : 0.5;
-var bigImageOffset = isMobile ? 0 : 50;
-var images = [];
-
-async function loadJSON(file) {
-  try {
-    console.log("Fetching json file: " + file);
-    // let jsonData = require(file);
-    const response = await fetch(file); // Adjust the path as needed
-    if (!response.ok) {
-      console.log("Error fetching json '" + file + "'", response);
-    }
-    const jsonData = await response.json(); // Parses the JSON data into a JS object
-    //console.log({ json: jsonData });
-    return jsonData;
-  } catch (error) {
-    console.error("Error fetching JSON file: "+ file, error);
-  }
-}
-
-function imgHoverOver() {
-	console.log(this.data.f);
-	this.animate({
-    	"left": "250px",
-    	"height": "+=150px",
-    	"width": "+=150px"
-	});
-}
-
-function imgOnLoad() {
-	var randomDelay = 100 * (Math.floor(Math.random() * 10)+1);
-	$(this).animate({
-		opacity: onImageLoadOpacity,
-		}, randomDelay);
-}
-
-function imgMouseOver() {
-	$(this).animate( {
-		"opacity": "1.0",
-		"width": "+=16px",
-		"height": "+=16px",
-		"left": "-=8px",
-		"top": "-=8px",
-		"zIndex": 20,
-		}, 200);
-}
-
-function imgMouseOut() {
-	$(this).animate( {
-		"opacity": "0.5",
-		"width": "-=16px",
-		"height": "-=16px",
-		"left": "+=8px",
-		"top": "+=8px",
-		"zIndex": 10,
-		}, "slow");
-}
-
-function showLoading() {
-	$("#preloader").animate( {
-		"opacity": '1.0',
-		}, "slow");
-}
-
-function hideLoading() {
-	$("#preloader").stop();
-	document.getElementById("preloader").style.opacity = "0.0";
-}
-
-function imgOnClick() {
-	hideBigImage();
-	var bg = document.getElementById("bg");
-	var img = document.createElement('img');
-	img.id = "scaledImage";
-	img.style.width = this.data.thumbnail.width + "px";
-	img.style.height = this.data.thumbnail.height + "px";
-	img.style.left = this.originalX + "px";
-	img.style.top = this.originalY + "px";
-	img.style.zIndex = 60;		
-	img.style.opacity = "0.0";
-	img.data = this.data;		
-	bg.appendChild(img);
-	img.src = this.src;
-	img.onclick = function() {
-		hideLoading();
-		var bg = document.getElementById("bg");
-		bg.removeChild(this);
+class CCUtil {
+	static async loadJSON(file) {
+	  try {
+	    console.log("Fetching json file: " + file);
+	    // let jsonData = require(file);
+	    const response = await fetch(file); // Adjust the path as needed
+	    if (!response.ok) {
+	      console.log("Error fetching json '" + file + "'", response);
+	    }
+	    const jsonData = await response.json(); // Parses the JSON data into a JS object
+	    //console.log({ json: jsonData });
+	    return jsonData;
+	  } catch (error) {
+	    console.error("Error fetching JSON file: "+ file, error);
+	  }
 	}
-	
-	var maxH = bg.clientHeight - 100;
-	var maxW = bg.clientWidth - 100;
-	var ow = this.data.original.width;
-	var oh = this.data.original.height;
-	var newW = ow;
-	var newH = oh;
+}
 
-	if (ow > oh) { // image is wider than tall
-		if (newW > maxW) {
-			var ratio = parseFloat(oh) / parseFloat(ow);
-			newW = maxW;
-			newH = maxW * ratio;
+class CCGalleryThumbnail {
+	ccGallery = null;
+	imgData = null;
+	x = null;
+	y = null;
+	thumbnailImage = null;
+
+	constructor(ccGallery, imgData, x, y) {
+		this.ccGallery = ccGallery;
+		this.imgData = imgData;
+		this.x = x;
+		this.y = y;
+		
+		this.thumbnailImage = this.createThumbnailImageElement(imgData, x, y);
+	}
+
+	createThumbnailImageElement(imgData, x, y) {
+		let ccThumbnail = this;
+
+		let thumbnailImg = document.createElement('img');		
+		thumbnailImg.ccThumbnail = this;
+
+		thumbnailImg.src = this.ccGallery.urlPrefix + "/" + imgData.thumbnail.file;
+		thumbnailImg.onload = this.handleOnLoad;
+		if (!ccGallery.isMobile) {
+			thumbnailImg.onmouseover = this.handleMouseOver;
+			thumbnailImg.onmouseout = this.handleMouseOut;
 		}
-		if (newH > maxH) { //if we're still too tall, let's scale the other way then
-			var ratio = parseFloat(ow) / parseFloat(oh);
-			newH = maxH;
-			newW = maxH * ratio;	
-		}
-	} else { //image is square or taller than wide
-		if (newH > maxH) {
-			var ratio = parseFloat(ow) / parseFloat(oh);
-			newH = maxH;
-			newW = maxH * ratio;	
-		}
-		if (newW > maxW) { //if we're too wide, let's scale the other way then
-			var ratio = parseFloat(oh) / parseFloat(ow);
-			newW = maxW;
-			newH = maxW * ratio;
-		}
+		thumbnailImg.onclick = this.handleOnClick;
+
+		thumbnailImg.style.width = imgData.thumbnail.width + "px";
+		thumbnailImg.style.height = imgData.thumbnail.height + "px";
+		thumbnailImg.style.left = x + "px";
+		thumbnailImg.style.top = y + "px";
+		thumbnailImg.style.opacity = "0.0";
+		thumbnailImg.style.zindex = 10;
+
+		this.thumbnailImg = thumbnailImg;
+
+		return thumbnailImg;
 	}
-	newW = Math.floor(newW);
-	newH = Math.floor(newH);
 
-	console.log("Original dims: " + this.data.original.width + "x" + this.data.original.height + ", Scaled: " + newW + "x" + newH);
-
-	var newX = Math.floor((bg.clientWidth - newW) / 2.0);
-	var newY = Math.floor((bg.clientHeight - newH) / 2.0);
-
-	console.log("New placement: " + newX + "x" + newY);
-
-	img.onload = function() {
-		$(this).animate( {
-		"opacity": "1.0",
-		"width": newW + "px",
-		"height": newH + "px",
-		"left": newX + "px",
-		"top": newY + "px",
-		}, 200, function() {
-			showLoading();
-			var overlay = document.getElementById("photooverlay");
-			overlay.style.width = newW + "px";
-			overlay.style.height = newH + "px";
-			overlay.style.left = (newX - bigImageOffset) + "px";
-			overlay.style.top = (newY - bigImageOffset) + "px";
-			overlay.style.opacity = "0.0";
-			overlay.style.zIndex = 200;
-			loadBigImage(this.data, newX, newY, newW, newH, img);
-		});
+	handleOnLoad() {
+		let ccGallery = this.ccThumbnail.ccGallery;
+		let randomDelay = 100 * (Math.floor(Math.random() * 10)+1);
+		$(this).animate({ opacity:ccGallery.onImageLoadOpacity }, randomDelay);
 	}
+
+	handleOnClick() {		
+		this.ccThumbnail.ccGallery.showBigImage(this.ccThumbnail);
+	}
+
+	handleMouseOver() {
+		let animationConfig = {
+			"opacity": "1.0",
+			"width": "+=16px",
+			"height": "+=16px",
+			"left": "-=8px",
+			"top": "-=8px",
+			"zIndex": 20,
+		}
+		$(this).animate(animationConfig, 200);
+	}
+
+	handleMouseOut() {
+		let animationConfig = {
+			"opacity": "0.5",
+			"width": "-=16px",
+			"height": "-=16px",
+			"left": "+=8px",
+			"top": "+=8px",
+			"zIndex": 10,
+		}
+		$(this).animate(animationConfig, "slow");
+	}	
 }
 
-function loadBigImage(imgData, newX, newY, newW, newH, scaledImage) {	
-	var img = document.createElement('img');
-	img.id = "bigImage";
-	img.style.width = newW + "px";
-	img.style.height = newH + "px";
-	img.style.left = newX + "px";
-	img.style.top = newY + "px";
-	img.style.zIndex = 70;		
-	img.data = imgData;		
-	bg.appendChild(img);
-	img.src = "gallery/" + imgData.original.file;
-	img.style.opacity = "0.0";
-	img.onclick = function() {
-		hideBigImage();
-	}
-	img.onload = function() {
-		hideLoading();
-		img.style.opacity = "1.0";
-	}
-}
+class CCGalleryBigImage {
+	ccGallery = null;
+	ccThumbnail = null;
+	bigImageConfig = null;
+	scaledImage = null;
+	bigImage = null;
 
-function hideBigImage() {
-	hideLastFloatingContent();
-	hideLoading();
-	var bg = document.getElementById("bg");
-	var scaledImage = document.getElementById("scaledImage");
-	if (scaledImage) {			
-		bg.removeChild(scaledImage);	
+	constructor(ccGallery, ccThumbnail) {
+		this.ccGallery = ccGallery;
+		this.ccThumbnail = ccThumbnail;
+		this.bigImageConfig = this.calculateBigImageConfig(ccGallery, ccThumbnail.imgData);
 	}
-	var bigImage = document.getElementById("bigImage");
-	if (scaledImage) {			
-		bg.removeChild(bigImage);	
-	}
-	var overlay = document.getElementById("photooverlay");
-	overlay.style.opacity = "0.0";
-	overlay.style.zIndex = 0;		
-}
 
-function resetImages() {
-	hideLastFloatingContent()
-	var bg = document.getElementById("bg");
-	while (bg.firstChild) {
-		bg.removeChild(bg.firstChild);
-	}
-	var overlay = document.getElementById("photooverlay");
-	overlay.onclick = function() {
-		hideBigImage();
-	}
-	var preloader = document.getElementById("preloader");
-	preloader.style.left = ((window.innerWidth - 32) / 2) + "px";
-	preloader.style.top = ((window.innerHeight - 32) / 2) + "px";
-	
-	var bgHeight = bg.clientHeight;
-	var bgWidth = bg.clientWidth;
-	var x = -50; 
-	var y = -50;
-	var imagesToUse = images.slice();
+	// first we show a image scaling operation that's just the 
+	// thumbnail scaling up to the "big image" size
+	showScaledImage() {
+		let scaledImage = document.createElement('img');
+		scaledImage.id = "scaledImage";
+		scaledImage.ccBigImage = this;
 
-	while (y < (bgHeight + 50)) {
-		while (x < bgWidth) {
-			var imgIndex = Math.floor(Math.random() * (imagesToUse.length));
-			var imgData = imagesToUse[imgIndex];
-			imagesToUse.splice(imgIndex, 1);
-			if (imagesToUse.length == 0) {
-				imagesToUse = images.slice();
+		scaledImage.src = this.ccGallery.urlPrefix + "/" + this.ccThumbnail.imgData.thumbnail.file;
+		scaledImage.onload = this.handleScaledImageLoad;
+		scaledImage.onclick = () => { this.ccBigImage.ccGallery.hideBigImage; };
+		
+		scaledImage.style.zIndex = 60;		
+		scaledImage.style.opacity = "0.0";
+		scaledImage.style.width = this.ccThumbnail.imgData.thumbnail.width + "px";
+		scaledImage.style.height = this.ccThumbnail.imgData.thumbnail.height + "px";
+		scaledImage.style.left = this.ccThumbnail.x + "px";
+		scaledImage.style.top = this.ccThumbnail.y + "px";		
+		
+		this.ccGallery.galleryContainer.appendChild(scaledImage);		
+
+		this.scaledImage = scaledImage;
+	}
+
+	handleScaledImageLoad() {
+		let ccBigImage = this.ccBigImage;
+		let ccGallery = this.ccBigImage.ccGallery;
+		ccGallery.showLoading();
+		ccGallery.showPhotoOverlay(this.ccBigImage.bigImageConfig);
+
+		let bigImageConfig = this.ccBigImage.bigImageConfig;		
+		let animationConfig = {
+			"opacity": "1.0",
+			"width": bigImageConfig.w + "px",
+			"height": bigImageConfig.h + "px",
+			"left": bigImageConfig.x + "px",
+			"top": bigImageConfig.y + "px",
+		};
+		
+		let animationCallback = () => { ccBigImage.showBigImage(); };
+		
+		$(this).animate(animationConfig, 200, animationCallback);
+	}
+
+	showBigImage() {
+		let bigImage = document.createElement('img');
+		bigImage.id = "bigImage";
+		bigImage.ccBigImage = this;
+
+		bigImage.src = this.ccGallery.urlPrefix + "/" + this.ccThumbnail.imgData.original.file;
+		bigImage.onload = this.handleBigImageLoad;
+		bigImage.onclick = () => { this.ccGallery.hideBigImage(); };
+		
+		bigImage.style.zIndex = 70;
+		bigImage.style.opacity = "0.0";
+		bigImage.style.width = this.bigImageConfig.w + "px";
+		bigImage.style.height = this.bigImageConfig.h + "px";
+		bigImage.style.left = this.bigImageConfig.x + "px";
+		bigImage.style.top = this.bigImageConfig.y + "px";
+
+		this.ccGallery.galleryContainer.appendChild(bigImage);
+
+		this.bigImage = bigImage;	
+	}
+
+	handleBigImageLoad() {
+		let ccGallery = this.ccBigImage.ccGallery;
+		ccGallery.hideLoading();
+		this.style.opacity = "1.0";
+	}
+
+	hide() {
+		if (this.scaledImage) {			
+			this.ccGallery.galleryContainer.removeChild(this.scaledImage);	
+		}
+		if (this.bigImage) {			
+			this.ccGallery.galleryContainer.removeChild(this.bigImage);	
+		}
+	}	
+
+	calculateBigImageConfig(ccGallery, imgData) {	
+		let galleryContainer = ccGallery.galleryContainer;
+		
+		var maxH = galleryContainer.clientHeight - 100;
+		var maxW = galleryContainer.clientWidth - 100;
+		var ow = imgData.original.width;
+		var oh = imgData.original.height;
+		var newW = ow;
+		var newH = oh;
+
+		if (ow > oh) { // image is wider than tall
+			if (newW > maxW) {
+				var ratio = parseFloat(oh) / parseFloat(ow);
+				newW = maxW;
+				newH = maxW * ratio;
 			}
-
-			var img = document.createElement('img');				
-			img.style.width = imgData.thumbnail.width + "px";
-			img.style.height = imgData.thumbnail.height + "px";
-			img.style.left = x + "px";
-			img.style.top = y + "px";
-			img.style.opacity = "0.0";
-			img.style.zindex = 10;
-			img.data = imgData;	
-			img.originalX = x;
-			img.originalY = y;			
-			
-			img.onload = imgOnLoad;
-			if (!isMobile) {
-				img.onmouseover = imgMouseOver;
-				img.onmouseout = imgMouseOut;
+			if (newH > maxH) { //if we're still too tall, let's scale the other way then
+				var ratio = parseFloat(ow) / parseFloat(oh);
+				newH = maxH;
+				newW = maxH * ratio;	
 			}
-			img.onclick = imgOnClick;			
-
-			bg.appendChild(img);				
-			img.src = "gallery/" + imgData.thumbnail.file;
-
-			x += imgData.thumbnail.width + 4;
+		} else { //image is square or taller than wide
+			if (newH > maxH) {
+				var ratio = parseFloat(ow) / parseFloat(oh);
+				newH = maxH;
+				newW = maxH * ratio;	
+			}
+			if (newW > maxW) { //if we're too wide, let's scale the other way then
+				var ratio = parseFloat(oh) / parseFloat(ow);
+				newW = maxW;
+				newH = maxW * ratio;
+			}
 		}
-		y += 104;
-		x = -50;
+		newW = Math.floor(newW);
+		newH = Math.floor(newH);
+
+		var newX = Math.floor((galleryContainer.clientWidth - newW) / 2.0);
+		var newY = Math.floor((galleryContainer.clientHeight - newH) / 2.0);
+
+		console.log("Original dims: " + ow + "x" + oh + ", Scaled: " + newW + "x" + newH);
+		console.log("New placement: " + newX + "x" + newY);
+
+		return { "x":newX, "y":newY, "w":newW, "h":newH };
 	}
 }
 
-async function firstLoadHandler() {
-	images = await loadJSON("gallery/galleries.json");
-	console.log("images", images);
-	resetImages();
-}
+class CCGallery {
+	containerId = null;
+	preloaderId = null;
+	photoOverlayId = null;
 
-window.onload = firstLoadHandler;
-window.onresize = resetImages;
+	isMobile = null;
+	onImageLoadOpacity = 1.0;
+	urlPrefix = null;	
+	
+	lastBigImage = null;
+	thumbnails = [];
 
-var lastFloatingContentId = null;
+	constructor(containerId, preloaderId, photoOverlayId) {
+		this.containerId = containerId;
+		this.preloaderId = preloaderId;
+		this.photoOverlayId = photoOverlayId;
 
-function hideLastFloatingContent() {
-	if (lastFloatingContentId != null) {
-		var content = document.getElementById(lastFloatingContentId);	
-		content.style.bottom = "-100%";
+		this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);		
+		this.onImageLoadOpacity = this.isMobile ? 1.0 : 0.5;
+		this.urlPrefix = "gallery"
+		console.log("isMobile: " + CCGallery.isMobile);
+	}
+
+	initializePageHandlers() {
+		window.ccGallery = this;
+		window.onload = this.firstLoadHandler;
+		window.onresize = this.resetImages;
+	}
+
+	showBigImage(ccThumbnail) {
+		this.hideBigImage();
+		let bigImage = new CCGalleryBigImage(this, ccThumbnail);
+		bigImage.showScaledImage();
+		this.lastBigImage = bigImage;
+	}	
+
+	hideBigImage() {
+		this.hideLoading();
+		if (this.lastBigImage) {
+			this.lastBigImage.hide();
+			this.lastBigImage = null;
+		}		
+		this.hidePhotoOverlay();		
+	}	
+
+	async firstLoadHandler() {
+		let ccGallery = window.ccGallery;
+		ccGallery.galleryContainer = document.getElementById(ccGallery.containerId);
+		
+		let data = await CCUtil.loadJSON(ccGallery.urlPrefix + "/galleries.json");
+		ccGallery.name = data.name;
+		ccGallery.images = data.images;
+		console.log("gallery name: " + ccGallery.name + ", images:", ccGallery.images);
+		
+		ccGallery.resetImages();
+	}
+
+	showLoading() {
+		$("#" + this.preloaderId).animate( { "opacity":'1.0' }, "slow");
+	}
+
+	hideLoading() {
+		$("#" + this.preloaderId).stop();
+		document.getElementById(this.preloaderId).style.opacity = "0.0";
+	}	
+
+	showPhotoOverlay(bigImageConfig) {
+		let overlay = document.getElementById(this.photoOverlayId);
+		overlay.style.width = bigImageConfig.w + "px";
+		overlay.style.left = bigImageConfig.x + "px";
+		overlay.style.top = (bigImageConfig.y + bigImageConfig.h) + "px";
+		overlay.style.opacity = "1.0";
+		overlay.style.zIndex = 200;
+		overlay.ccGallery = this;
+		overlay.onclick = () => { this.ccGallery.hideBigImage; };
+	}
+
+	hidePhotoOverlay(bigImageConfig) {
+		let overlay = document.getElementById(this.photoOverlayId);
+		overlay.style.opacity = "0.0";
+		overlay.style.zIndex = 0;		
+	}
+
+	resetImages() {
+		// remove all children of container		
+		while (this.galleryContainer.firstChild) {
+			this.galleryContainer.removeChild(this.galleryContainer.firstChild);
+		}
+
+		// re-center preloader in case browser resized
+		let preloader = document.getElementById(this.preloaderId);
+		preloader.style.left = ((window.innerWidth - 32) / 2) + "px";
+		preloader.style.top = ((window.innerHeight - 32) / 2) + "px";
+		
+		let containerHeight = this.galleryContainer.clientHeight;
+		let containerWidth = this.galleryContainer.clientWidth;
+		let currentThumbnailX = -50; 
+		let currentThumbnailY = -50;
+		let imagesToUse = this.images.slice();
+
+		while (currentThumbnailY < (containerHeight + 50)) {
+			while (currentThumbnailX < containerWidth) {
+				let imgIndex = Math.floor(Math.random() * (imagesToUse.length));
+				let imgData = imagesToUse[imgIndex];
+				imagesToUse.splice(imgIndex, 1);
+				if (imagesToUse.length == 0) {
+					imagesToUse = this.images.slice();
+				}
+
+				let ccThumbnail = new CCGalleryThumbnail(this, imgData, currentThumbnailX, currentThumbnailY);
+				this.galleryContainer.appendChild(ccThumbnail.thumbnailImage);				
+				this.thumbnails.push(ccThumbnail);
+
+				currentThumbnailX += imgData.thumbnail.width + 4;
+			}
+			currentThumbnailY += 104;
+			currentThumbnailX = -50;
+		}
 	}
 }
 
-function showFloatingContent(contentId) {
-	hideBigImage();
-	lastFloatingContentId = contentId;
-	var content = document.getElementById(contentId);
-	$("#" + contentId).animate({ bottom:35 + "px" }, {duration: 1500 });		
+class CCGalleryInfo {
+	constructor(name, images) {
+		this.name = name;
+		this.images = images;
+	}
 }
+
+var gallery = new CCGallery("bg", "preloader", "photooverlay");
+gallery.initializePageHandlers();
